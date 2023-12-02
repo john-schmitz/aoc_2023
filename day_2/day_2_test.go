7e7a6589c2e2a001bdd9beebd7a650328ca1de49
john <johngc2010@hotmail.com>
2023-12-02 12:58:33 -0300

    refactor

--- day_2/day_2_test.go
+++ day_2/day_2_test.go
@@ -4,16 +4,14 @@ import (
 	"testing"
 )
 
-type testpair struct {
-	input  string
-	output bool
-}
-
 const SAMPLE_PATH = "sample.txt"
 const INPUT_PATH = "input.txt"
 
 func TestPossibleGames(t *testing.T) {
-	test_cases := []testpair{
+	test_cases := []struct {
+		input  string
+		output bool
+	}{
 		{"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", true},
 		{"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", true},
 		{"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", false},
