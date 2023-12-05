6c5d11e2522d00bc09435900a492d599e16f494d
John Schmitz <john.schmitz@ada.tech>
2023-12-05 08:36:00 -0300

    part 2 sample data

--- day_5/day_5.go
+++ day_5/day_5.go
@@ -108,6 +108,44 @@ func PartOne(file_path string) int {
 	return lowest_location_number
 }
 
+func PartTwo(file_path string) int {
+	lowest_location_number := math.MaxInt
+	seeds, steps, err := parseInput(file_path)
+
+	seeds = convertSeedsToRanges(seeds)
+	seed_steps := make([][]int, len(seeds))
+
+	for i, seed := range seeds {
+		seed_steps[i] = make([]int, len(steps)+1)
+		seed_steps[i][0] = seed
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	for step_index, step := range steps {
+	seed_label:
+		for index, seed := range seeds {
+			for _, ranges := range step {
+				maped_seed := mapRange(seed, ranges)
+				seeds[index] = maped_seed
+				seed_steps[index][step_index+1] = maped_seed
+				if seed != maped_seed {
+					continue seed_label
+				}
+			}
+		}
+	}
+
+	for _, seed := range seed_steps {
+		last_step := seed[len(seed)-1]
+		lowest_location_number = min(last_step, lowest_location_number)
+	}
+
+	return lowest_location_number
+}
+
 func parseInput(file_path string) ([]int, [][]Ranges, error) {
 	steps := make([][]Ranges, 7)
 	current_step_index := -1
@@ -149,4 +187,23 @@ func parseInput(file_path string) ([]int, [][]Ranges, error) {
 	}
 
 	return seeds, steps, nil
+}
+
+func convertSeedsToRanges(seeds []int) []int {
+	if len(seeds)%2 != 0 {
+		panic("seeds must be even")
+	}
+
+	output := make([]int, 0)
+
+	for i := 0; i < len(seeds); i = i + 2 {
+		start := seeds[i]
+		length := seeds[i+1]
+
+		for j := 0; j < length; j++ {
+			output = append(output, start+j)
+		}
+	}
+
+	return output
 }
--- day_5/day_5_test.go
+++ day_5/day_5_test.go
@@ -13,11 +13,28 @@ func TestPartOne(t *testing.T) {
 		expected  int
 	}{
 		{sample_path, 35},
-		{input_path, 35},
+		{input_path, 551761867},
 	}
 
 	for _, test_case := range test_cases {
 		actual := PartOne(test_case.file_path)
+		if actual != test_case.expected {
+			t.Errorf("Expected %v, got %v", test_case.expected, actual)
+		}
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	test_cases := []struct {
+		file_path string
+		expected  int
+	}{
+		{sample_path, 46},
+		{input_path, 46},
+	}
+
+	for _, test_case := range test_cases {
+		actual := PartTwo(test_case.file_path)
 		if actual != test_case.expected {
 			t.Errorf("Expected %v, got %v", test_case.expected, actual)
 		}
@@ -64,5 +81,31 @@ func TestMapRange(t *testing.T) {
 		if actual != test_case.output {
 			t.Errorf("Expected %v, got %v", test_case.output, actual)
 		}
+	}
+}
+
+func TestConvertSeedsToRanges(t *testing.T) {
+	test_cases := []struct {
+		seeds    []int
+		expected []int
+	}{
+		{[]int{79, 14, 55, 13},
+			[]int{79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67},
+		},
+	}
+
+	for _, test_case := range test_cases {
+		actual := convertSeedsToRanges(test_case.seeds)
+
+		if len(actual) != len(test_case.expected) {
+			t.Errorf("Expected %v, got %v", test_case.expected, actual)
+		}
+
+		for i := range test_case.expected {
+			if test_case.expected[i] != actual[i] {
+				t.Errorf("Expected %v, got %v", test_case.expected, actual)
+			}
+		}
+
 	}
 }
