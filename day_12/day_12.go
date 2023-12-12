62f9fce31b37cdbbda450048c41777176845fbfb
John Schmitz <john.schmitz@ada.tech>
2023-12-12 16:29:02 -0300

    wuo

---
+++ day_12/day_12.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func toIntSlice(input string) []int {
+	output := make([]int, 0)
+
+	for _, v := range strings.Split(input, ",") {
+		number, err := strconv.Atoi(v)
+		if err != nil {
+			panic(err)
+		}
+
+		output = append(output, number)
+	}
+
+	return output
+}
+
+func appendAtStart(s string, value string) string {
+	if len(s) < 1 {
+		panic("String has no length")
+	}
+
+	if len(value) != 1 {
+		panic("Invalid length")
+	}
+
+	return s[:0] + value + s[0+1:]
+}
+
+func solve(expression string, groups []int) int {
+	if expression == "" && len(groups) > 0 {
+		return 0
+	}
+
+	if expression == "" && len(groups) == 0 {
+		return 1
+	}
+
+	first_char := string(expression[0])
+	current_group := groups[0]
+
+	if first_char == "?" {
+		return solve(appendAtStart(expression, "#"), groups) + solve(appendAtStart(expression, "."), groups)
+	}
+
+	if first_char == "." {
+		return solve(expression[1:], groups)
+	}
+
+	if first_char == "#" {
+		if current_group > len(expression) {
+			return 0
+		}
+		for i := 0; i < current_group; i++ {
+			if string(expression[i]) == "." {
+				return solve(expression[i:], groups)
+			}
+		}
+
+		x := expression[current_group:]
+
+		if x == "" {
+			return solve(x, groups[1:])
+		}
+
+		return solve(x[current_group:], groups[1:])
+	}
+
+	return 0
+}
+
+func PossibleArrangements(line string) int {
+	slitted := strings.Split(line, " ")
+	numbers := toIntSlice(slitted[1])
+
+	return solve(slitted[0], numbers)
+}
---
+++ day_12/day_12_test.go
@@ -0,0 +1,50 @@
+package main
+
+import "testing"
+
+
+func TestPossibleArrangements(t *testing.T) {
+	testCases := []struct {
+		line     string
+		expected int
+	}{
+		{
+			line:     "?.?.### 1,1,3",
+			expected: 1,
+		},
+		{
+			line:     "???.### 1,1,3",
+			expected: 1,
+		},
+		// {
+		// 	line:     "????.#...#... 4,1,1",
+		// 	expected: 1,
+		// },
+		// {
+		// 	line:     "????.######..#####. 1,6,5",
+		// 	expected: 4,
+		// },
+		// {
+		// 	line:     ".??..??...?##. 1,1,3",
+		// 	expected: 4,
+		// },
+		// {
+		// 	line:     "?#?#?#?#?#?#?#? 1,3,1,6",
+		// 	expected: 1,
+		// },
+		// {
+		// 	line:     "?###???????? 3,2,1",
+		// 	expected: 10,
+		// },
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.line, func(t *testing.T) {
+			actual := PossibleArrangements(tC.line)
+
+			if actual != tC.expected {
+				t.Errorf("PossibleArrangements(%s) = %d. Got %d", tC.line, tC.expected, actual)
+			}
+		})
+	}
+}
