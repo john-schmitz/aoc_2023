3f6e03b851f32378bc84cb364c1bff503c87860f
johngc2010@hotmail.com <johngc2010@hotmail.com>
2023-12-20 00:24:17 -0300

    part one

--- day_19/day_19.go
+++ day_19/day_19.go
@@ -2,7 +2,6 @@ package day_19
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -12,17 +11,56 @@ type Part struct {
 	x, m, a, s int
 }
 
+func (part Part) Sum() int {
+	return part.x + part.a + part.m + part.s
+}
+
 func PartOne(input_file string) int {
 	lines, err := getLines(input_file)
 	if err != nil {
 		panic(err)
 	}
 
+	parts := []Part{}
+	expressions := map[string]string{}
+	parsing_expressions := true
+
 	for _, line := range lines {
-		fmt.Println(line)
+		if line == "" {
+			parsing_expressions = false
+			continue
+		}
+		if parsing_expressions {
+			index := strings.IndexAny(line, "{")
+			expression := line[index:]
+			expression = expression[1:][:len(expression)-2]
+			token := line[:index]
+			expressions[token] = expression
+		} else {
+			part, err := parsePart(line)
+			if err != nil {
+				panic(err)
+			}
+
+			parts = append(parts, part)
+		}
+	}
+	acc := 0
+	for i := 0; i < len(parts); i++ {
+		start := "in"
+		part := parts[i]
+		result := evaluateWorkflow(part, expressions[start])
+
+		for result != "A" && result != "R" {
+			result = evaluateWorkflow(part, expressions[result])
+		}
+
+		if result == "A" {
+			acc = acc + part.Sum()
+		}
 	}
 
-	return 0
+	return acc
 }
 
 func getLines(path string) ([]string, error) {
@@ -63,4 +101,65 @@ func parsePart(line string) (Part, error) {
 	}
 
 	return Part{x, m, a, s}, nil
+}
+
+func getOperator(expression string) string {
+	if strings.Contains(expression, "<") {
+		return "<"
+	}
+
+	return ">"
+}
+
+func getComparator(part Part, expression string) int {
+	if strings.Contains(expression, "a") {
+		return part.a
+	}
+
+	if strings.Contains(expression, "s") {
+		return part.s
+	}
+
+	if strings.Contains(expression, "m") {
+		return part.m
+	}
+
+	return part.x
+}
+
+func evaluateExpression(part Part, expression string) bool {
+	operator := getOperator(expression)
+
+	result := strings.Split(expression, operator)
+	parsedInt, err := strconv.Atoi(result[1])
+	if err != nil {
+		panic(err)
+	}
+
+	comparator := getComparator(part, expression)
+
+	if operator == ">" {
+		return comparator > parsedInt
+	}
+
+	return comparator < parsedInt
+}
+
+func evaluateWorkflow(part Part, line string) string {
+	expressions := strings.Split(line, ",")
+	for _, expression := range expressions {
+		if !strings.Contains(expression, ":") {
+			return expression
+		}
+
+		splited := strings.Split(expression, ":")
+
+		next_token := splited[1]
+
+		if evaluateExpression(part, splited[0]) {
+			return next_token
+		}
+	}
+
+	panic("end of workflow")
 }
--- day_19/day_19_test.go
+++ day_19/day_19_test.go
@@ -1,6 +1,9 @@
 package day_19
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Test(t *testing.T) {
 	testCases := []struct {
@@ -9,7 +12,11 @@ func Test(t *testing.T) {
 	}{
 		{
 			desc:     "sample.txt",
-			expected: 200,
+			expected: 19114,
+		},
+		{
+			desc:     "input.txt",
+			expected: 432788,
 		},
 	}
 	for _, tC := range testCases {
@@ -45,6 +52,131 @@ func TestParsePart(t *testing.T) {
 			actual, _ := parsePart(tC.input)
 			if actual != tC.expected {
 				t.Errorf("Expected ParsePart(%s) = %v. Got %v", tC.input, tC.expected, actual)
+			}
+		})
+	}
+}
+
+func TestPartSum(t *testing.T) {
+	testCases := []struct {
+		input    Part
+		expected int
+	}{
+		{
+			input:    Part{787, 2655, 1222, 2876},
+			expected: 7540,
+		},
+		{
+			input:    Part{2036, 264, 79, 2244},
+			expected: 4623,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(fmt.Sprintf("%v", tC.input), func(t *testing.T) {
+
+			if tC.input.Sum() != tC.expected {
+				t.Errorf("Expected Sum of %v = %d Got %d", tC.input, tC.expected, tC.input.Sum())
+			}
+		})
+	}
+}
+
+func TestEvaluateExpression(t *testing.T) {
+	testCases := []struct {
+		expression string
+		part       Part
+		expected   bool
+	}{
+		{
+			expression: "a<2006",
+			part:       Part{x: 0, m: 0, a: 100, s: 0},
+			expected:   true,
+		},
+		{
+			expression: "a>2006",
+			part:       Part{x: 0, m: 0, a: 100, s: 0},
+			expected:   false,
+		},
+		{
+			expression: "m<20",
+			part:       Part{x: 0, m: 200, a: 0, s: 0},
+			expected:   false,
+		},
+		{
+			expression: "m>10",
+			part:       Part{x: 0, m: 11, a: 0, s: 0},
+			expected:   true,
+		},
+		{
+			expression: "s<20",
+			part:       Part{s: 200},
+			expected:   false,
+		},
+		{
+			expression: "s>10",
+			part:       Part{s: 11},
+			expected:   true,
+		},
+		{
+			expression: "x<20",
+			part:       Part{x: 200},
+			expected:   false,
+		},
+		{
+			expression: "x>10",
+			part:       Part{x: 11},
+			expected:   true,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.expression, func(t *testing.T) {
+			actual := evaluateExpression(tC.part, tC.expression)
+
+			if actual != tC.expected {
+				t.Errorf("Expected evaluateExpression(%v, %s) = %t. Got %t", tC.part, tC.expression, tC.expected, actual)
+			}
+		})
+	}
+}
+
+func TestWorkflow(t *testing.T) {
+	testCases := []struct {
+		expression string
+		part       Part
+		expected   string
+	}{
+		{
+			expression: "a<2006:qkq,m>2090:A,rfg",
+			part:       Part{x: 1679, m: 44, a: 2067, s: 496},
+			expected:   "rfg",
+		},
+		{
+			expression: "s<1351:px,qqz",
+			part:       Part{x: 787, m: 2655, a: 1222, s: 2876},
+			expected:   "qqz",
+		},
+		{
+			expression: "s<1351:px,qqz",
+			part:       Part{x: 2461, m: 1339, a: 466, s: 291},
+			expected:   "px",
+		},
+		{
+			expression: "m>1548:A,A",
+			part:       Part{x: 2461, m: 1339, a: 466, s: 291},
+			expected:   "A",
+		},
+		{
+			expression: "m>1548:R,R",
+			part:       Part{x: 2461, m: 1339, a: 466, s: 291},
+			expected:   "R",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.expression, func(t *testing.T) {
+			actual := evaluateWorkflow(tC.part, tC.expression)
+
+			if actual != tC.expected {
+				t.Errorf("Expected evaluateWorkflow(%v, %s) = %s. Got %s", tC.part, tC.expression, tC.expected, actual)
 			}
 		})
 	}
